b454279c8f2dc1e4cfe93708c468633f6901f182
Sara Hassan <sarahassan@MacBook-Pro-7.local>
2023-08-30 12:46:34 -0600

    Our fifth commit

--- src/Day01/Animal.java
+++ src/Day01/Animal.java
@@ -4,4 +4,7 @@ public class Animal {
     public static void main(String[] args) {
         System.out.println("Hello guys");
     }
+    public void method1(){
+        System.out.println("Hello World");
+    }
 }
--- src/Day01/Student.java
+++ src/Day01/Student.java
@@ -1,6 +1,8 @@
 package Day01;
 
 public class Student {
+    int age;
+    String name;
     public static void main(String[] args) {
         for (int i = 0; i < 10; i++){
             System.out.println(i);
