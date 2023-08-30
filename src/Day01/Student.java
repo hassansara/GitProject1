57d0f4ece7d98c90ab3b6896c9b1fc01a89fd008
Sara Hassan <sarahassan@MacBook-Pro-7.local>
2023-08-30 12:26:48 -0600

    Our second change

--- src/Day01/Student.java
+++ src/Day01/Student.java
@@ -1,4 +1,9 @@
 package Day01;
 
 public class Student {
+    public static void main(String[] args) {
+        for (int i = 0; i < 10; i++){
+            System.out.println(i);
+        }
+    }
 }
